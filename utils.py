01e2312cbfa0a4aa43ea649f26480089882422e3
Woojun Yang <yangwj@dgist.ac.kr>
2026-05-12 11:16:41 +0900

    Change sorting parameter in arXiv API query

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def request_paper_with_arXiv_api(keyword: str, max_results: int, link: str = "OR
     # keyword = keyword.replace(" ", "+")
     assert link in ["OR", "AND"], "link should be 'OR' or 'AND'"
     keyword = "\"" + keyword + "\""
-    url = "http://export.arxiv.org/api/query?search_query=ti:{0}+{2}+abs:{0}&max_results={1}&sortBy=lastUpdatedDate".format(keyword, max_results, link)
+    url = "http://export.arxiv.org/api/query?search_query=ti:{0}+{2}+abs:{0}&max_results={1}&sortBy=submittedDate".format(keyword, max_results, link)
     url = urllib.parse.quote(url, safe="%/:=&?~#+!$,;'@()*[]")
     response = urllib.request.urlopen(url).read().decode('utf-8')
     feed = feedparser.parse(response)
